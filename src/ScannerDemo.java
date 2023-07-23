cd1578306e4dc298ea56bb7c24d7f30f8e6526c2
Harjeet167 <surajkumar202323@gmail.com>
2023-07-23 09:35:14 +0530

    scanner class

---
+++ src/ScannerDemo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String str=sc.nextLine();
+        System.out.println("Please enter your age");
+        int a = sc.nextInt();
+        System.out.println("Pleae enter your mobile number");
+        long mob= sc.nextLong();
+        System.out.println("Please enter your adhar number");
+        long ad= sc.nextLong();
+        System.out.println("Please enter your Account number");
+        long ac= sc.nextLong();
+
+
+    }
+}
