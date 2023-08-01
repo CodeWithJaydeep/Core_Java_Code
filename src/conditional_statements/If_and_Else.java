50ebd22c12284532b026167552bec47b13b1b70b
Harjeet167 <surajkumar202323@gmail.com>
2023-08-02 01:51:37 +0530

    conditional statements (If and else)

---
+++ src/conditional_statements/If_and_Else.java
@@ -0,0 +1,20 @@
+package conditional_statements;
+
+public class If_and_Else {
+    public static void main(String[] args) {
+        int age=18;
+        if (age==18){
+            System.out.println("You can drive ! ");
+        }
+        else {
+            System.out.println("You can not drive !");
+        }
+        int age1=19;
+        if (age1>18){
+            System.out.println("You are eligible for Voting");
+        }
+        else {
+            System.out.println("You are not elegible for voting");
+        }
+    }
+}
