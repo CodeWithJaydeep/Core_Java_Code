de10a9701d5a903ec530941d9844e9886630431f
Harjeet167 <surajkumar202323@gmail.com>
2023-08-02 16:21:42 +0530

    if else

---
+++ src/Practice/If_ElseDemo.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class If_ElseDemo {
+    public static void main(String[] args) {
+
+        int Price =180000;
+        if (Price>18000){
+            System.out.println("Price is correct !");
+        }
+        else {
+            System.out.println("Price is not correct !");
+        }
+        if (Price==180000){
+            System.out.println("equal");
+        }
+        else {
+            System.out.println("Not equal");
+        }
+
+    }
+}
