d976c1338cf2f05847d1be3a4a405d3afec7386f
Harjeet167 <surajkumar202323@gmail.com>
2023-08-02 16:21:11 +0530

    Multi

---
+++ src/Operators/Multi.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class Multi {
+    public static void main(String[] args) {
+        int a=100;
+        int b = 200*a;
+        System.out.println(b);
+
+        int a1=1000;
+        int b1 =500;
+        int c=a1*b1;
+        System.out.println(c);
+
+
+    }
+}
