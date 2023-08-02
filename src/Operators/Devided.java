6750df46f1a653b121bdfd4453d67e825a63c151
Harjeet167 <surajkumar202323@gmail.com>
2023-08-02 16:20:44 +0530

    Devided

---
+++ src/Operators/Devided.java
@@ -0,0 +1,29 @@
+package Operators;
+
+public class Devided {
+    public static void main(String[] args) {
+
+        //First way Devided
+        int a=200;;
+        int b=100;
+        System.out.println(a/b);
+
+        // Second way Devided
+
+        int a1=200;;
+        int b1=100;
+        System.out.println(a1/b1);
+
+        //Third way Devided
+
+        int a2=200;;
+        int b2=100;
+        int c2=a2/b2;
+        System.out.println(a1/b2);
+
+
+
+
+
+    }
+}
