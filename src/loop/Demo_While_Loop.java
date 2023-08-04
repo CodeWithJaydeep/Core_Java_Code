b96128ff31bfae4a00ea8e2177340864d8b4e008
Harjeet167 <surajkumar202323@gmail.com>
2023-08-04 13:53:13 +0530

    While Loop

---
+++ src/loop/Demo_While_Loop.java
@@ -0,0 +1,24 @@
+package loop;
+
+public class Demo_While_Loop {
+
+    public static void main(String[] args) {
+
+        // 1 to 100 Counting.......
+        int i=0;
+        while(i<=100){
+            System.out.print(","+i);
+            i++;
+        }
+        System.out.println();
+        // 100 to 1 Counting........
+
+        int j=100;
+        while(j>=0) {
+            System.out.print(","+j);
+            j--;
+        }
+    }
+
+    }
+
